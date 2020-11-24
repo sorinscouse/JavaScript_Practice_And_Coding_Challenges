b995b3a2b1ef317519c8ccdc76c313c05653a5c9
sorinscouse <sorin.olteanu@cst-dev.com>
2020-11-24 21:33:23 +0200

    updated

---
+++ 01-Fundamentals-Part 1/05-Coding Challenge  1/script.js
@@ -0,0 +1,17 @@
+// Data 1
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+//Data 2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
