2be043ac2586223d8cee5d9e9d0e09e5cde9f866
sorinscouse <sorin.olteanu@cst-dev.com>
2020-11-25 12:44:46 +0200

    Handling an Esc Keypress Event

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -21,3 +21,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key); //With this we can see in the console what key we pressed
+
+  //if the pressed key is 'Esc' and if modal doesn't contain the 'hidden' class, then close the modal
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      //execute this function to close the modal
+      closeModal();
+  }
+});
