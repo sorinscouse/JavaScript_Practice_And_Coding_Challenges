8786ea68b32526a67de36b2f275a57c316e79bdf
sorinscouse <sorin.olteanu@cst-dev.com>
2020-11-25 17:04:24 +0200

    Put the scores to 0 and make te dice disappear

--- 06-Dice Game/script.js
+++ 06-Dice Game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//Put the scores to 0 and make te dice disappear
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
