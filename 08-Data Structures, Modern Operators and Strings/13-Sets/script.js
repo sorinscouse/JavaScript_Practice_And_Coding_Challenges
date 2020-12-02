9fff99bcdbcd6ace745d6fdcf506c15aed6e68aa
sorinscouse <sorin.olteanu@cst-dev.com>
2020-12-02 18:00:34 +0200

    Sets

---
+++ 08-Data Structures, Modern Operators and Strings/13-Sets/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ordersSet = new Set([
+    'Pasta',
+    'Pizza',
+    'Pizza',
+    'Risotto',
+    'Pasta',
+    'Pizza',
+]);
+
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+console.log(ordersSet);
+ordersSet.delete('Risotto');
+console.log(ordersSet);
+// ordersSet.clear();
+// console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+
+console.log(staffUnique);
+
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('Sorin').size);
