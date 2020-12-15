079c7ee1ff501faef65b8f9f64abdf2670759857
sorinscouse <sorin.olteanu@cst-dev.com>
2020-12-15 19:32:53 +0200

    Sticky navigation - Intersection Observer API

---
+++ 12-Advanced DOM and Events/06-Sticky navigation - Intersection Observer API/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const obsCallback = function (entries, observer) {
+    entries.forEach(entry => {
+        console.log(entry);
+    });
+};
+
+const obsOptions = {
+    root: null,
+    threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
