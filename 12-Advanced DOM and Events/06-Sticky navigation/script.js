f24c95fb1a5e6a6cc1eff088bbf57ab7883d2de0
sorinscouse <sorin.olteanu@cst-dev.com>
2020-12-15 22:19:04 +0200

    Lifecycle DOM Events

---
+++ 12-Advanced DOM and Events/06-Sticky navigation/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+    console.log(window.scrollY);
+
+    if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+});
+
