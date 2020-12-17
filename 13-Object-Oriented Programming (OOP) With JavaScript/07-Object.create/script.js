4711b1521b1633363530868b7055f441ab1a09dc
sorinscouse <sorin.olteanu@cst-dev.com>
2020-12-17 13:37:33 +0200

    Object.create

---
+++ 13-Object-Oriented Programming (OOP) With JavaScript/07-Object.create/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+};
+
+const steven = Object.create(PersonProto); // 'Object.create' creates a new object and the prototype of that object will be the object that we pass in
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+console.log(sarah);
